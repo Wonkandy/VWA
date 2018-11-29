0a6007f098ce48de10d902ca87a24f538941f4dd
unknown <ynoshd@gmail.com>
2018-11-29 10:56:51 +0100

    Test

--- NNForms/Form1.cs
+++ NNForms/Form1.cs
@@ -150,7 +150,7 @@ namespace NNForms
             }
             */
             int[] i = Helper.ConvertBitmap(m);
-
+            //Test
             ResetDrawing();
 
             string result = lb ? lable.Value + "," : "";
